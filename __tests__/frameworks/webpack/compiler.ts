d783445cdea67fbd9f6d3fc4ebffc17414b97bff
Ivan Demchuk <ivan.demchuk@gmail.com>
2022-07-20 17:20:45 +0300

    Add webpack tests

---
+++ __tests__/frameworks/webpack/compiler.ts
@@ -0,0 +1,47 @@
+import path from 'path'
+import webpack from 'webpack'
+import { Volume, createFsFromVolume } from 'memfs'
+
+import { VueLoaderPlugin } from 'vue-loader'
+
+import type { UserOptions } from '../../../src'
+import { webpackPlugin } from '../../../src'
+
+export default async (fixture: string, options: Partial<UserOptions> = {}, hot = false): Promise<webpack.Stats> => {
+  const compiler = webpack({
+    context: path.resolve(__dirname, '../..'),
+    entry: `./${fixture}`,
+    output: {
+      path: path.resolve(__dirname),
+      filename: 'bundle.js',
+    },
+    mode: 'production',
+    module: {
+      rules: [
+        {
+          test: /\.vue$/,
+          loader: 'vue-loader',
+        },
+      ],
+    },
+    plugins: [
+      new VueLoaderPlugin(),
+      webpackPlugin(options),
+      ...(hot ? [new webpack.HotModuleReplacementPlugin()] : []),
+    ],
+  })
+
+  compiler.outputFileSystem = createFsFromVolume(new Volume())
+  compiler.outputFileSystem.join = path.join.bind(path)
+
+  return await new Promise((resolve, reject) => {
+    compiler.run((err, stats) => {
+      if (err != null || stats == null)
+        return reject(err)
+      if (stats.hasErrors())
+        return reject(stats.toJson().errors)
+
+      resolve(stats)
+    })
+  })
+}
