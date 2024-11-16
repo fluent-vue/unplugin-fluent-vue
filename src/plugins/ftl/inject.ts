7b9c8bfce921ba2ad263c65b68a2191830a9210d
Ivan Demchuk <ivan.demchuk@gmail.com>
2024-11-16 17:08:53 +0200

    Simplify injecting FTL code

--- src/plugins/ftl/inject.ts
+++ src/plugins/ftl/inject.ts
@@ -5,6 +5,10 @@ import MagicString from 'magic-string'
 
 type InjectFtlFn = (template: TemplateStringsArray, locale?: string, source?: string) => MagicString
 
+function normalize(str: string) {
+  return str.replace(/\r\n/g, '\n').trim()
+}
+
 export function getInjectFtl(options: SFCPluginOptions): InjectFtlFn {
   return (template, locale, source) => {
     if (source == null) {
@@ -15,21 +19,16 @@ export function getInjectFtl(options: SFCPluginOptions): InjectFtlFn {
     if (source == null)
       throw new Error('Missing source')
 
-    let magic = new MagicString(source)
+    const magic = new MagicString(source)
     const importString = options.parseFtl === true ? '' : '\nimport { FluentResource } from \'@fluent/bundle\'\n'
     const localeString = locale == null ? '' : locale
 
     if (options.parseFtl === true) {
-      const resource = new FluentResource(source)
+      const resource = new FluentResource(normalize(source))
       magic.overwrite(0, source.length, JSON.stringify(resource))
     }
     else {
-      // Escape string
-      magic.replace(/"/g, '\\"')
-      magic.replace(/\n/g, '\\n')
-      magic = magic.snip(1, -1)
-      magic.prepend('new FluentResource("')
-      magic.append('")')
+      magic.overwrite(0, source.length, `new FluentResource(${JSON.stringify(normalize(source))})`)
     }
 
     magic.prepend(importString + template[0] + localeString + template[1])
